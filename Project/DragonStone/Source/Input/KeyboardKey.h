9379e426aef5ebaf8f8cba3f863e34e9557a1a5b
Joseph Stuhr <JpDeathBlade@gmail.com>
2013-05-18 13:34:56 -0400

    Added KeyboardKey enum. Updated comments for MouseButton.

--- Project/DragonStone/Source/Input/KeyboardKey.h
+++ Project/DragonStone/Source/Input/KeyboardKey.h
@@ -1,6 +1,6 @@
 /*
  * 0===========================================================================0
- * | MouseButton.h                                                             |
+ * | KeyboardKey.h                                                             |
  * |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  |
  * |   Author        : Joseph Stuhr                                            |
  * |   E-mail        : JpDeathBlade@gmail.com                                  |
@@ -9,7 +9,7 @@
  * 0=====0===============================================================0=====0
  * | Code Description:                                                         |
  * |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  |
- * |   An enum for all the buttons on a mouse.                                 |
+ * |   An enum for all the buttons on a keyboard.                              |
  * 0=====0===============================================================0=====0
  *       |                                                               |
  * 0=====0===============================================================0=====0
@@ -37,20 +37,119 @@
  * 0===========================================================================0
  */
 
-#ifndef  DRAGONSTONE_MOUSEBUTTON_H_		// Check to see if this .h is defined
-#define  DRAGONSTONE_MOUSEBUTTON_H_		//  if not, define it
+#ifndef  DRAGONSTONE_KEYBOARDKEY_H_		// Check to see if this .h is defined
+#define  DRAGONSTONE_KEYBOARDKEY_H_		//  if not, define it
 
 namespace DragonStone
 {
-	enum MouseButton
+	enum KeyboardKey
 	{
+		Unknown = -1,
+		
+		A = 0,
+		B,
+		C,
+		D,
+		E,
+		F,
+		G,
+		H,
+		I,
+		J,
+		K,
+		L,
+		M,
+		N,
+		O,
+		P,
+		Q,
+		R,
+		S,
+		T,
+		U,
+		V,
+		W,
+		X,
+		Y,
+		Z,
+		Num0,
+		Num1,
+		Num2,
+		Num3,
+		Num4,
+		Num5,
+		Num6,
+		Num7,
+		Num8,
+		Num9,
+		Escape,
+		LControl,
+		LShift,
+		LAlt,
+		LSystem,
+		RControl,
+		RShift,
+		RAlt,
+		RSystem,
+		Menu,
+		LBracket,
+		RBracket,
+		SemiColon,
+		Comma,
+		Period,
+		Quote,
+		Slash,
+		BackSlash,
+		Tilde,
+		Equal,
+		Dash,
+		Space,
+		Return,
+		BackSpace,
+		Tab,
+		PageUp,
+		PageDown,
+		End,
+		Home,
+		Insert,
+		Delete,
+		Add,
+		Subtract,
+		Multiply,
+		Divide,
 		Left,
 		Right,
-		Middle,
-		XButton1,
-		XButton2,
-		ButtonCount
+		Up,
+		Down,
+		Numpad0,
+		Numpad1,
+		Numpad2,
+		Numpad3,
+		Numpad4,
+		Numpad5,
+		Numpad6,
+		Numpad7,
+		Numpad8,
+		Numpad9,
+		F1,
+		F2,
+		F3,
+		F4,
+		F5,
+		F6,
+		F7,
+		F8,
+		F9,
+		F10,
+		F11,
+		F12,
+		F13,
+		F14,
+		F15,
+		Pause,
+
+		KeyCount
 	};
 }
 
-#endif //DRAGONSTONE_MOUSEBUTTON_H_
+#endif //DRAGONSTONE_KEYBOARDKEY_H_
